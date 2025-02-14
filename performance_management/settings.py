f4f2a7011e181f126a2c2a56dba68802c832838e
Sakshi <gatesakship@example.com>
2025-02-14 23:13:42 +0530

    Added Render domain to ALLOWED_HOSTS

--- performance_management/settings.py
+++ performance_management/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['performance-management-1.onrender.com', '127.0.0.1']
